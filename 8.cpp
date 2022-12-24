375eff37597a1090b8e181b2794b65b58e7c41e3
AmritPaudel <46588187+amrit073@users.noreply.github.com>
2022-12-24 16:45:42 +0545

    testworkflow

--- 8.cpp
+++ 8.cpp
@@ -1,5 +1,6 @@
-// 8. Write a program defining the class Distance with data member feet and inch as integer.
-//    Overload the constructors along with copy constructor. Use the member functions for input and
-//    output as well as to compute the sum of the two objects and return the sum. Show read/write
-//    operations in main() using above information.
-
+// 8. Write a program defining the class Distance with data member feet and inch
+// as integer.
+//    Overload the constructors along with copy constructor. Use the member
+//    functions for input and output as well as to compute the sum of the two
+//    objects and return the sum. Show read/write operations in main() using
+//    above information.
