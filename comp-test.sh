693eeffc521bbbdb937e5471c9d90759dc32298c
AmritPaudel <46588187+amrit073@users.noreply.github.com>
2023-01-06 21:54:47 +0545

    mock

--- comp-test.sh
+++ comp-test.sh
@@ -1,7 +1,7 @@
 # #!/bin/bash
 # for i in $(find . -type f -name "*.c") ;do if grep math.h "$i">/dev/null; then gcc -o out $i -lm ;else gcc -o out $i; fi ; done && for i in $(find . -type f -name "*.cpp") ; do g++ -o out $i  ; done  | grep error
 #!/bin/bash
-
+set -e
 compile () {
   find . -type f -name "*.c" | while read -r i
   do 
