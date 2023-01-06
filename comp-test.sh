98a55a92e3354cf1b699404260ccf003789bf4bc
AmritPaudel <46588187+amrit073@users.noreply.github.com>
2023-01-06 21:28:59 +0545

    mock

--- comp-test.sh
+++ comp-test.sh
@@ -2,21 +2,21 @@
 # for i in $(find . -type f -name "*.c") ;do if grep math.h "$i">/dev/null; then gcc -o out $i -lm ;else gcc -o out $i; fi ; done && for i in $(find . -type f -name "*.cpp") ; do g++ -o out $i  ; done  | grep error
 #!/bin/bash
 for i in $(find . -type f -name "*.c")
-        do 
-                if grep math.h "$i">/dev/null
-                then gcc -o out "$i" -lm 
-                else gcc -o out "$i"
-                fi 
-        done 
+do 
+  if grep math.h "$i" 2>/dev/null
+  then gcc -o out "$i" -lm  2>/dev/null
+  else gcc -o out "$i" 2>/dev/null
+  fi 
+done 
 for i in $(find . -type f -name "*.cpp") 
-do g++ -o out "$i" 
+do g++ -o out "$i"  2>/dev/null
 done  
-
-if [ $? -eq 0 ] 
-then 
-  echo "Successfully compiled" 
-  exit 0 
-else 
-  echo "Could compile program" >&2 
-  exit 1 
-fi
+#
+# if [ $? -eq 0 ] 
+# then 
+#   echo "Successfully compiled" 
+#   exit 0 
+# else 
+#   echo "Could compile program" >&2 
+#   exit 1 
+# fi
